feeaa98994d4c50ae082d19dd18547f67aff9033
MadDogNelson <kay@maddog-development.com>
2016-04-24 14:00:09 +0200

    First commit

---
+++ Emailer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emailer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                string mail_from = "kay@maddog-development.com";
+                string mail_to = "kay.nelson@hotmail.se";
+                string rubrik = "Test";
+                string message = "Test";
+                //
+                //
+                //
+
+                using (var client = new System.Net.Mail.SmtpClient("localhost", 25))
+                {
+                    client.UseDefaultCredentials = true;
+                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    client.Send(mail_from, mail_to, rubrik, message);
+                }
+                
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine(ex.InnerException.Message);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
